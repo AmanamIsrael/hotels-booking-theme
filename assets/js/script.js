f13029745be2e0a652f2ac293df19bbab75a689d
Amanam Israel <skaiflex6@gmail.com>
2020-06-08 16:27:31 +0100

    homepage, navbar and footer added

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+
+    let buttonEl = document.getElementById("#toggle");
+    
+        function toogleNav() {
+            const linksEl = document.querySelector(".links");
+            linksEl.classList.toggle("show");
+            console.log('yo')
+        }
+    
+    
+  
